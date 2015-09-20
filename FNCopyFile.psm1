45a0712ff2ed420d8c2a7e6bafec7b585c2a3e53
FaustoNascimento <faustoconsultancy@outlook.com>
2015-09-21 00:37:15 +0200

    Update FNCopyFile.psm1

--- FNCopyFile.psm1
+++ FNCopyFile.psm1
@@ -21,15 +21,12 @@ function Copy-FNFileToSession
 		[Int] $BufferSize = 4MB,
 
 		[Parameter(ValueFromPipelineByPropertyName)]
-		[Switch] $Force,
-
-		[Parameter(ValueFromPipelineByPropertyName)]
-		[Int] $FileLockedMaxTries = 100
+		[Switch] $Force
 	)
 
 	Begin
 	{
-		function InternalCopyFile
+		function InternalCopyToSession
 		{
 			[CmdletBinding()]
 
@@ -46,80 +43,26 @@ function Copy-FNFileToSession
 
 				[Int] $BufferSize,
 
-				[Switch] $Force,
-
-				[Int] $FileLockedMaxTries
+				[Switch] $Force
 			)
 
-			$scriptBlockChecks = {
-				$destinationFolder = ([IO.FileInfo] $using:destinationFile).Directory.FullName
-			
-				if (-not (Test-Path -Path $destinationFolder -PathType Container))
-				{
-					if ($using:Force)
-					{
-						[void] (New-Item -Path $destinationFolder -ItemType Directory -Force -ErrorAction Stop)
-					}
-					else
-					{
-						Write-Error 'Destination folder not found'
-						return
-					}
-				}
+            $scriptBlockOpenFile = {
+	            if ((Test-Path -Path $using:DestinationFile -PathType Leaf) -and -not $using:Force)
+	            {
+		            if (-not $using:Force)
+		            {
+			            Write-Error 'File already exists.'
+			            return
+		            }
+	            }
 
-				if ((Test-Path -Path $using:destinationFile -PathType Leaf) -and -not $using:Force)
-				{
-					Write-Error 'File already exists.'
-					return
-				}
-			}
+	            $writeStream = ([IO.FileInfo]$using:DestinationFile).Open([IO.FileMode]::Create)
+            }
 
-			$scriptBlockCopy = {
-				try
+			$scriptBlockCloseFile = {
+				if ($writeStream)
 				{
-					if ($using:create)
-					{
-						[IO.File]::WriteAllBytes($using:DestinationFile, $using:buffer)
-					}
-					else
-					{
-						# If explorer is open on the folder we're copying the files to, it can have the file open for reading to determine it's size, 
-						# causing an error because the file is open by another process
-						# So if it fails to create the FileStream, we wait a bit and try again.
-						$tries = 0
-						
-						do
-						{
-							try
-							{
-								$writeStream = New-Object IO.FileStream($using:DestinationFile, [IO.FileMode]::Append)
-							}
-							catch
-							{
-								$tries += 1
-
-								if ($tries -ge $using:FileLockedMaxTries)
-								{
-									throw $_
-								}
-
-								Start-Sleep -Milliseconds 10
-							}
-						} while (-not $writeStream)
-
-						$writeStream.Write($using:buffer, 0, $using:buffer.Length)
-					}
-				}
-				catch
-				{
-					Write-Error -Exception $_.Exception.InnerException
-				}
-				finally
-				{
-					if ($writeStream)
-					{
-						$writeStream.Close()
-					}
+					$writeStream.Close()
 
 					[GC]::Collect()
 				}
@@ -127,10 +70,16 @@ function Copy-FNFileToSession
 
 			try
 			{
-				Invoke-Command -Session $Session -ScriptBlock $scriptBlockChecks -ErrorAction Stop
-				
-				$create = $true
+				Invoke-Command -Session $Session -ScriptBlock $scriptBlockOpenFile -ErrorAction Stop
+
 				$readStream = [IO.File]::OpenRead($SourceFile)
+				
+				# Initial buffer size
+				if ($BufferSize -gt $readStream.Length)
+				{
+					$BufferSize = $readStream.Length
+				}
+
 				$buffer = New-Object byte[] $BufferSize
 				
 				while ($readStream.Length -gt $readStream.Position) 
@@ -150,11 +99,8 @@ function Copy-FNFileToSession
 						$bytesRead += $readStream.Read($buffer, $bytesRead, $BufferSize - $bytesRead)
 					}
 
-					Invoke-Command -Session $Session -ScriptBlock $scriptBlockCopy -ErrorAction Stop
-					Write-Progress -Activity "Copying $SourceFile to $DestinationFile over WinRM, BufferSize = $BufferSize" -PercentComplete ($readStream.Position / $readStream.Length * 100) -Status "$($readStream.Position) / $($readStream.Length) bytes processed" -ParentId 1
-
-					# Set flag to false, to stop attempting to create the file (again)
-					$create = $false
+					Invoke-Command -Session $Session -ScriptBlock {$writeStream.Write($using:buffer, 0, $using:BufferSize)} -ErrorAction Stop
+					Write-Progress -Activity "Copying $SourceFile to $DestinationFile over WinRM, BufferSize = $BufferSize" -PercentComplete ($readStream.Position / $readStream.Length * 100) -Status "$($readStream.Position) / $($readStream.Length) bytes processed"
 				}
 			}
 			catch
@@ -167,6 +113,8 @@ function Copy-FNFileToSession
 				{
 		            $readStream.Close()
 	            }
+
+				Invoke-Command -Session $Session -ScriptBlock {$scriptBlockCloseFile}
 			}
 		}
 	}
@@ -180,39 +128,51 @@ function Copy-FNFileToSession
 			return
 		}
 
-		# If we specified $ComputerName instead of a session, let's open the session now
-		if ($ComputerName)
-		{
-			$Session = New-PSSession -ComputerName $ComputerName -ErrorVariable sessionError -ErrorAction SilentlyContinue
-
-			if ($sessionError)
-			{
-				$PSCmdlet.WriteError((New-ErrorRecord -ErrorMessage $sessionError.Exception.Message))
-				return
-			}
-		}
-		
+		# We have the try up here so we can have a finally block where we check if the session needs closing
+		# This means that even if the command is aborted, the finally statement is always executed and we don't leave PSSessions lingering around
 		try
 		{
+			# If we specified $ComputerName instead of a session, let's open the session now
+			if ($ComputerName)
+			{
+				$Session = New-PSSession -ComputerName $ComputerName -ErrorVariable sessionError -ErrorAction SilentlyContinue
+
+				if ($sessionError)
+				{
+					$PSCmdlet.WriteError((New-ErrorRecord -ErrorMessage $sessionError.Exception.Message))
+					return
+				}
+			}
+			
 			# If $source is a container
 			if (Test-Path $Source -PathType Container)
 			{
-				$files = Get-ChildItem -Path $Source -Recurse -File -Force
+				$directories = Get-ChildItem -Path $Source -Recurse -Directory -Force
+                
+                foreach ($directory in $directories)
+                {
+                    $destinationDirectory = Join-Path $Destination $directory.FullName.SubString($Source.Length)
 
-				for ($i = 0; $i -lt $files.Count; $i++)
-				{
-					# Build the destination file path, based on the relative $Source path
-					Push-Location $Source
-					$destinationFile = Join-Path $Destination (Resolve-Path $files[$i] -Relative)
-					Pop-Location
-				
-					Write-Progress -Activity "Copying files from $Source to remote computer $($Session.ComputerName)" -Status "Processing file $file" -CurrentOperation "$i / $($files.Count)" -PercentComplete ($i / $files.Count * 100) -Id 1
-					InternalCopyFile -Source $files[$i].FullName -Destination $destinationFile -BufferSize $BufferSize -Force:$Force -FileLockedMaxTries $FileLockedMaxTries -Session $Session
-				}
+                    Invoke-Command -Session $Session -ScriptBlock `
+                    {
+                        if (-not (Test-Path $using:destinationDirectory -PathType Container))
+                        {
+                            [void] (New-Item -Path $using:destinationDirectory -ItemType Directory -Force -ErrorAction Stop)
+                        }
+                    }
+
+                    $files = Get-ChildItem -Path $directory.FullName -File -Force
+
+                    foreach ($file in $files)
+                    {
+                        $destinationFile = Join-Path $destinationDirectory $file.Name
+                        InternalCopyToSession -Source $file.FullName -Destination $destinationFile -BufferSize $BufferSize -Force:$Force -Session $Session
+                    }
+                }
 			}
 			else
 			{
-				InternalCopyFile -Source $Source -Destination $Destination -BufferSize $BufferSize -Force:$Force -FileLockedMaxTries $FileLockedMaxTries -Session $Session
+				InternalCopyToSession -SourceFile $Source -DestinationFile $Destination -BufferSize $BufferSize -Force:$Force -Session $Session
 			}
 		}
 		catch
@@ -227,6 +187,208 @@ function Copy-FNFileToSession
 				Remove-PSSession -Session $Session
 			}
 		}
+	}
+}
+
+function Copy-FNFileFromSession
+{
+	[CmdletBinding()]
+
+	param
+	(
+		[Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
+		[Alias('PSPath')]
+		[String] $Source,
+
+		[Parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 1)]
+		[String] $Destination,
+
+		[Parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 2, ParameterSetName = 'ComputerName')]
+		[String] $ComputerName,
+
+		[Parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 2, ParameterSetName = 'Session')]
+		[System.Management.Automation.Runspaces.PSSession] $Session,
+
+		[Parameter(ValueFromPipelineByPropertyName)]
+		[Int] $BufferSize = 4MB,
+
+		[Parameter(ValueFromPipelineByPropertyName)]
+		[Switch] $Force
+	)
+
+    Begin
+	{
+		function InternalCopyFromSession
+		{
+			[CmdletBinding()]
+
+			param
+			(
+				[Parameter(Mandatory)]
+				[String] $SourceFile,
+
+				[Parameter(Mandatory)]
+				[String] $DestinationFile,
+
+				[Parameter(Mandatory)]
+				[System.Management.Automation.Runspaces.PSSession] $Session,
+
+				[Int] $BufferSize,
+
+				[Switch] $Force
+			)
+
+            $scriptBlockCloseFile = {
+				if ($readStream)
+				{
+					$readStream.Close()
+
+					[GC]::Collect()
+				}
+			}
+
+            try
+            {
+                if ((Test-Path -Path $DestinationFile -PathType Leaf) -and -not $Force)
+	            {
+		            if (-not $Force)
+		            {
+			            Write-Error 'File already exists.'
+			            return
+		            }
+	            }
+
+	            $writeStream = ([IO.FileInfo]$DestinationFile).Open([IO.FileMode]::Create)
+
+                $totalBytes = Invoke-Command -Session $Session -ScriptBlock `
+                {
+                    $readStream = [IO.File]::OpenRead($using:SourceFile)
+
+                    #Initial buffer size
+                    $bufferSize = $using:BufferSize
+
+                    if ($bufferSize -gt $readStream.Length)
+				    {
+					    $bufferSize = $readStream.Length
+				    }
+
+				    $buffer = New-Object byte[] $bufferSize
+
+                    $readStream.Length
+                }
+
+                $bytesWritten = 0
+
+                while ($bytesWritten -lt $totalBytes)
+                {
+                    Invoke-Command -Session $Session -ScriptBlock `
+                    {
+                        # If the number of remaining bytes to read is lower than the buffer size, we set our buffer size accordingly and redim our array
+					    if ($bufferSize -gt $readStream.Length - $readStream.Position)
+					    {
+						    $bufferSize = $readStream.Length - $readStream.Position
+						    $buffer = New-Object byte[] $bufferSize
+					    }
+
+                        $bytesRead = 0
+
+                        # We do this to ensure that we always have a full buffer array
+					    while ($bytesRead -lt $bufferSize)
+					    {
+						    $bytesRead += $readStream.Read($buffer, $bytesRead, $bufferSize - $bytesRead)
+					    }
+                        
+                        # Cheap way to stop the pipeline from expanding the $buffer
+                        @{'buffer' = $buffer}
+                    } | ForEach-Object {
+                        $writeStream.Write($_.Buffer, 0, $_.Buffer.Length)
+                        
+                        $bytesWritten += $_.Buffer.Length
+                        Write-Progress -Activity "Copying $SourceFile to $DestinationFile over WinRM, BufferSize = $($_.Buffer.Length)" -PercentComplete ($bytesWritten / $totalBytes * 100) -Status "$bytesWritten / $totalBytes bytes processed"
+                    }
+                }
+            }
+            catch
+			{
+				throw $_
+			}
+			finally
+			{
+				if ($writeStream)
+				{
+		            $writeStream.Close()
+	            }
+
+				Invoke-Command -Session $Session -ScriptBlock {$scriptBlockCloseFile}
+			}
+        }
+    }
+
+    Process
+	{
+        # We have the try up here so we can have a finally block where we check if the session needs closing
+		# This means that even if the command is aborted, the finally statement is always executed and we don't leave PSSessions lingering around
+		try
+		{
+			# If we specified $ComputerName instead of a session, let's open the session now
+			if ($ComputerName)
+			{
+				$Session = New-PSSession -ComputerName $ComputerName -ErrorVariable sessionError -ErrorAction SilentlyContinue
+
+				if ($sessionError)
+				{
+					$PSCmdlet.WriteError((New-ErrorRecord -ErrorMessage $sessionError.Exception.Message))
+					return
+				}
+			}
+
+            # If $source doesn't exist, exit
+		    if (-not (Invoke-Command -Session $Session -ScriptBlock {Test-Path $using:Source}))
+		    {
+			    $PSCmdlet.WriteError((New-ErrorRecord -ErrorMessage "Source path not found: '$Source'" -ErrorCategory ObjectNotFound))
+			    return
+		    }
+
+            # If $source is a container
+            if (Invoke-Command -Session $Session -ScriptBlock {Test-Path $using:Source -PathType Container})
+            {
+                $directories = Invoke-Command -Session $Session -ScriptBlock {Get-ChildItem -Path $using:Source -Recurse -Directory -Force}
+
+                foreach ($directory in $directories)
+                {
+                    $destinationDirectory = Join-Path $Destination $directory.FullName.SubString($Source.Length)
+                    
+                    if (-not (Test-Path $destinationDirectory -PathType Container))
+                    {
+                        [void] (New-Item -Path $destinationDirectory -ItemType Directory -Force -ErrorAction Stop)
+                    }
+
+                    $files = Invoke-Command -Session $Session -ScriptBlock {Get-ChildItem -Path $using:directory.FullName -File -Force}
+
+                    foreach ($file in $files)
+                    {
+                        $destinationFile = Join-Path $destinationDirectory $file.Name
+                        InternalCopyFromSession -SourceFile $file.FullName -DestinationFile $destinationFile -BufferSize $BufferSize -Force:$Force -Session $Session
+                    }
+                }
+            }
+            else
+            {
+                InternalCopyFromSession -SourceFile $Source -DestinationFile $Destination -BufferSize $BufferSize -Force:$Force -Session $Session
+            }
+        }
+        catch
+		{
+			throw $_
+		}
+		finally
+		{
+			# If we created our own PSSession, let's close it.
+			if ($ComputerName)
+			{
+				Remove-PSSession -Session $Session
+			}
+        }
 	}
 }
 
